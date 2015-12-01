18c025a3d039911963285aaeaa7fa317c30ad43e
shub1905 <shub1905@gmail.com>
2015-11-30 20:16:48 -0500

    roughly complete

--- psi_gen.py
+++ psi_gen.py
@@ -7,7 +7,9 @@ import randomness
 block_s_metric = defaultdict()
 Data = data_generation.data()
 random_s_block = defaultdict()
-R = 10 #to be fixed
+final_metric = defaultdict()
+R = 10  # to be fixed
+
 
 def s_vals():
   s_val = []
@@ -21,7 +23,7 @@ def s_vals():
   return s_val
 
 
-def gen_shifts_block(Data):
+def gen_shifts_block():
   possible_s = s_vals()
 
   for x_number, x in enumerate(Data):
@@ -41,6 +43,31 @@ def all_random_numbers():
   for block_number in range(len(partitions)):
     for s in possible_s:
       key = '{}_{}'.format(block_number, s)
-      I_arr = randomness.gen_random(s, len(x), R)
-      H_arr = randomness.hash(s, R)
+      # TODO discuss dim - s
+      I_arr = randomness.gen_random(s, len(x) - s, R)
+      H_arr = randomness.hash(s, len(x) - s, R)
       random_s_block[key] = (I_arr, H_arr)
+
+
+def final_4d_metric():
+  for key in block_s_metric:
+    [x_ind, x_block_ind, s] = map(int, key.split('_'))
+    rand_key = '{}_{}'.format(x_block_ind, s)
+    (I_arr, H_arr) = random_s_block[rand_key]
+
+    for u in range(R):
+      for v in range(4 * s):
+        key_final = '{}_{}_{}'.format(key, u, v)
+        final_metric[key_final] = psi(R, s, H_arr[u], I_arr[u], key)
+
+
+def psi(r, s, H, I, key):
+  result = set()
+  x_ss = block_s_metric[key]
+  for j in range(s):
+    x_new = [x_ss[j][ri] for ri in I]
+    x_new = ''.join(x_new)
+    x_new = int(x_new, 2)
+    result.add(H[x_new])
+
+  return len(result)*1.0 / 2*r
--- randomness.py
+++ randomness.py
@@ -10,12 +10,16 @@ def gen_random(s, d, r):
   I_arr = []
   for i in range(0, r * b, b):
     I_arr.append(list(choices[i:i + b]))
+  return I_arr
 
-  print I_arr
 
-
-def hash(s, r):
-  return list(numpy.random.choice(range(1, 4 * s + 1), r))
+def hash(s, d, r):
+  b_ = 2**(int(math.ceil(d * math.log(4 * s) / s)))
+  choices = list(numpy.random.choice(range(1, 4 * s + 1), r * b_))
+  H_arr = []
+  for i in range(0, r * b_, b_):
+    H_arr.append(list(choices[i:i + b_]))
+  return H_arr
 
 if __name__ == '__main__':
   gen_random(10, 16, 4, 8)
