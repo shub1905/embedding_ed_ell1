777a47605ae2b7cabe5676a3e70d6ace2eafc4d1
shub1905 <shub1905@gmail.com>
2015-12-08 23:36:56 -0500

    minor changes to protein read

--- protein_read.py
+++ protein_read.py
@@ -1,22 +1,26 @@
 import sys
 
 
-def read_file_protein(file_handle):
+def read_file_protein(file_name):
+    if file_name == None:
+        file_name = 'raw_data/UniProt.txt'
+
+    file_handle = open(file_name, 'r')
     header_line = file_handle.readline()
     proteins = []
     for line in file_handle:
         line = line.replace('\n', '')
         proteins.append(line.split('\t')[-1])
+
+    file_handle.close()
     return proteins
 
 
 if __name__ == '__main__':
+    file_name = None
     if len(sys.argv) < 2:
-        print '''usage: python protein_read.py file_name'''
-        sys.exit(0)
-
-    file_name = sys.argv[1]
-    file_handle = open(file_name, 'r')
-    proteins = read_file_protein(file_handle)
-    print proteins[:5]
-    file_handle.close()
+        file_name = 'raw_data/UniProt.txt'
+    else:
+        file_name = sys.argv[1]
+    proteins = read_file_protein(None)
+    print proteins
