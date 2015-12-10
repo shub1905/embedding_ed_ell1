063e40bbbd7e3fdd705e3881ce285f11aedaa3a7
Patanjali <v.patanjali@gmail.com>
2015-12-09 19:17:56 -0500

    Linear scan and comparison utility

--- plots.py
+++ plots.py
@@ -10,6 +10,8 @@ import numpy
 from matplotlib import pyplot
 import pandas
 
+RESULTS_DIR = 'results/'
+
 #%% Plotting runtimes
 
 data = pandas.read_csv('data_norm.time', delimiter='\t')
@@ -18,25 +20,25 @@ pyplot.figure(2)
 pyplot.plot(data['dimension'], data['embedding time'],'r+')
 pyplot.xlabel('Dimension')
 pyplot.ylabel('Embedding time')
-pyplot.savefig('Embedding_time.png')
+pyplot.savefig(RESULTS_DIR+'Embedding_time.png')
 
 pyplot.figure(3)
 pyplot.plot(data['dimension'], data['l1 time'],'r+')
 pyplot.xlabel('Dimension')
 pyplot.ylabel('Time to calculate l1')
-pyplot.savefig('l1.png')
+pyplot.savefig(RESULTS_DIR+'l1.png')
 
 pyplot.figure(4)
 pyplot.plot(data['dimension'], data['edit_time'],'r+')
 pyplot.xlabel('Dimension')
 pyplot.ylabel('Time to calculate edit distance')
-pyplot.savefig('edit.png')
+pyplot.savefig(RESULTS_DIR+'edit.png')
 
 pyplot.figure(5)
 pyplot.plot(data['l1 time'], data['edit_time'],'r+')
 pyplot.xlabel('Time to calculate l1 distance')
 pyplot.ylabel('Time to calculate edit distance')
-pyplot.savefig('edit_vs_l1.png')
+pyplot.savefig(RESULTS_DIR+'edit_vs_l1.png')
 
 #%% Plotting distortions
 time_dict = {}
@@ -49,7 +51,5 @@ for files in os.listdir('distances/'):
         [size, dimension, delta] = map(float, params[2:5])
         if dimension <= 20 and size > 2**dimension:
             size = 2**dimension
-        if 'arr_1' in dist.keys():
-            temp = dist['arr_1'].tolist()
-            time_dict[files] = temp
-            norm_factor = size * (size - 1) / 2
+        if 'arr_0' in dist.keys():
+            data = dist['arr_0']
---
+++ utils.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  9 17:57:20 2015
+
+@author: patanjali
+"""
+
+import numpy
+import numpy.linalg
+from matplotlib import pyplot
+
+def l_1(x, y, dtype = 'numpy'):
+    if dtype == 'list':
+        x = numpy.array(x)
+        y = numpy.array(y)
+        dtype = 'numpy'
+    if dtype == 'numpy':
+        return numpy.linalg.norm(x-y,1)
+    
+
+def l_2(x, y, dtype = 'numpy'):
+    if dtype == 'list':
+        x = numpy.array(x)
+        y = numpy.array(y)
+        dtype = 'numpy'
+    if dtype == 'numpy':
+        return numpy.linalg.norm(x-y,2)
+    
+
+def nearest_neighbours_linear_scan(queries, database, distance_function, iterator_type = 'list'):
+    
+    if queries is database:
+        ignore_self = True
+        print "Making all vs all comparison"
+        
+    if iterator_type in ['list', 'numpy']:
+        output = numpy.zeros((len(queries), 2))
+        for query_index, query in enumerate(queries):
+            smallest_distance = float('inf')
+            nearest_neighbour = 0
+            for index, neighbour in enumerate(database):
+                if ignore_self:
+                    if query_index == index:
+                        continue
+                distance = distance_function(query, neighbour)
+                if distance < smallest_distance:
+                    smallest_distance = distance
+                    nearest_neighbour = index
+            #print nearest_neighbour, smallest_distance
+            output[query_index,0] = nearest_neighbour
+            output[query_index,1] = smallest_distance
+            
+    return output
+    
+def compare_nearest_neighbours(queries, database, base_metric,
+                               base_metric_result, embedded_metric_result,
+                               print_summary = True):
+
+    """
+    Returns an array with [embedded_nearest_neighbour, embedded_distance, 
+                              edit_distance_to_actual_nearest_neighbour,
+                              edit_distance_to_embedded_nearest_neighbour
+                              ]
+    for each of the query points
+    """
+    
+    output = numpy.zeros((embedded_metric_result.shape[0],4))
+    output[:, 0:2] = embedded_metric_result
+    output[:, 2] = base_metric_result[:,1]
+    for query_index, query in enumerate(queries):
+        output[query_index, 3] = base_metric(queries[query_index], 
+                                            database[int(embedded_metric_result[query_index, 0])])
+    
+    if print_summary == True:
+        distance_distribution = (output[:,2]/output[:,3])
+        distance_distribution.sort()
+        print distance_distribution.shape
+        pyplot.plot(distance_distribution, 
+                    numpy.arange(distance_distribution.shape[0])*1.0/distance_distribution.shape[0])
+        pyplot.savefig('nn_compare_dist.png')
+    return output
